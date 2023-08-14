19910e21ef625f684f30a15b3042697c3a53d497
Ankush2811 <ankushawsm@gmail.com>
2023-08-14 08:53:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var inputText = document.querySelector('#usertext')
+var outputText = document.querySelector('#translation')
+var buttonCall = document.querySelector('#btn-translate')
+var api = "https://api.funtranslations.com/translate/minion.json";
+
+function fetchData(){
+  let userSpeak = inputText.value
+  fetch(`${api}?text=${userSpeak}`)
+  .then((response) => response.json())
+  .then((data)=>{
+    outputText.innerHTML = data.contents.translated
+  })
+  .catch((error) => console.log(error));
+}
+
+buttonCall.addEventListener("click", fetchData);
